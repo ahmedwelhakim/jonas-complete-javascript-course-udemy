a247646d55289decdd6da563510206c305a9b14b
ahmedwelhakim <ahmed.w.elhakim@gmail.com>
2022-08-01 02:08:45 +0200

    adding game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const input = document.querySelector('.guess');
+const checkBtn = document.querySelector('.btn.check');
+const message = document.querySelector('.message');
+const scoreOutput = document.querySelector('.score');
+const rand = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+checkBtn.addEventListener('click', () => {
+	const number = Number(input.value);
+	if (number > rand) {
+		message.textContent = 'Too High';
+		scoreOutput.textContent = --score;
+	}
+	else if (number < rand) {
+		message.textContent = 'Too Low';
+		scoreOutput.textContent = --score;
+	}
+	else {
+		message.textContent = 'Correct';
+		scoreOutput.textContent = score;
+	}
+})
