0a78ae94361d6fad20a0779eebf6e3a166300424
ahmedwelhakim <ahmed.w.elhakim@gmail.com>
2022-08-03 01:55:43 +0200

    finishing modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const showModal = () => {
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+}
+const closeModal = () => {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+}
+
+for (const btn of showModalBtns) {
+	btn.addEventListener('click', showModal);
+}
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', (e) => {
+	console.log(e.key);
+	if (e.key === 'Escape') closeModal();
+});
