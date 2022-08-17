b988c9e7197a2d297599eed2b10d6a91554d34b1
ahmedwelhakim <ahmed.w.elhakim@gmail.com>
2022-08-17 20:04:57 +0200

    implementing The Bankist app in 11-Array-Bankist

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,6 +71,106 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+const createUserNames = function (accs) {
+  for (const acc of accs) {
+    acc.username = acc.owner.toLowerCase().split(' ').reduce((acc, curr) => acc + curr[0], '');
+  }
+}
+// Functions Implementation
+const isValidAuth = function (accs, username, pin) {
+  return accs.map(v => v.username).includes(username) ? accs.map(v => v.pin).includes(+pin) : false;
+}
+
+const getUserAccountIndex = function (accs, username) {
+  return accs.map(v => v.username).indexOf(username);
+}
+const showUI = function () {
+  containerApp.style.opacity = 1;
+}
+const hideUI = function () {
+  containerApp.style.opacity = 0;
+}
+const login = function (accounts, username) {
+  const loggedUser = accounts[getUserAccountIndex(accounts, username)];
+  labelWelcome.textContent = `Welcome, ${loggedUser.owner.split(' ')[0]}`;
+  updateUI(loggedUser);
+  showUI();
+  return loggedUser;
+}
+const logout = function (accounts, username) {
+  accounts.splice(getUserAccountIndex(accounts, username), 1);
+  labelWelcome.textContent = `Log in to get started`;
+  hideUI();
+}
+
+const displayMovements = function (acc) {
+  containerMovements.innerHTML = '';
+  acc.movements.forEach((v, i) => {
+    const type = v > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${v}€</div>
+        </div>`;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+}
+const calcAndDisplayBalance = function (acc) {
+  labelBalance.textContent = acc.movements.reduce((prev, curr) => (prev + curr), 0) + '€';
+}
+const calcAndDisplaySummary = function (acc) {
+  labelSumIn.textContent = acc.movements.filter(v => v > 0).reduce((prev, curr) => (prev + curr), 0) + '€';
+  labelSumOut.textContent = acc.movements.filter(v => v < 0).reduce((prev, curr) => (prev + Math.abs(curr)), 0) + '€';
+  labelSumInterest.textContent = acc.movements.
+    filter(v => v > 0).
+    map(v => (v * +acc.interestRate) / 100).
+    filter(v => v >= 1).
+    reduce((p, v) => p + v, 0) + '€';
+}
+const updateUI = function (loggedAcc) {
+  displayMovements(loggedAcc);
+  calcAndDisplayBalance(loggedAcc);
+  calcAndDisplaySummary(loggedAcc);
+}
+const transfer = function (userAcc, receiverAcc, amount) {
+  if (Number.parseInt(labelSumIn.textContent) >= amount) {
+    userAcc.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    updateUI(userAcc);
+  }
+}
+const loan = function (userAcc, amount) {
+  if (userAcc.movements.some(v => v >= amount * 0.1)) {
+    userAcc.movements.push(amount);
+    updateUI(userAcc);
+  }
+}
+// Create userName with first initials
+createUserNames(accounts);
+
+// Authentication
+let loggedUser = null;
+btnLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (isValidAuth(accounts, inputLoginUsername.value, inputLoginPin.value)) {
+    loggedUser = login(accounts, inputLoginUsername.value);
+  }
+})
+btnTransfer.addEventListener('click', (e) => {
+  e.preventDefault();
+  transfer(loggedUser, accounts[getUserAccountIndex(accounts, inputTransferTo.value)], +inputTransferAmount.value);
+})
+btnLoan.addEventListener('click', e => {
+  e.preventDefault();
+  loan(loggedUser, +inputLoanAmount.value);
+})
+btnClose.addEventListener('click', e => {
+  e.preventDefault();
+  if (isValidAuth(accounts, inputCloseUsername.value, +inputClosePin.value)) {
+    logout(accounts, inputCloseUsername.value);
+  }
+})
