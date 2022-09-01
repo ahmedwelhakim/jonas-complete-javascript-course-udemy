786dd082e1a586da638fc3654dff39df79065ba7
ahmedwelhakim <ahmed.w.elhakim@gmail.com>
2022-09-01 22:53:33 +0200

    Implementing Challenge 3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -9,3 +9,26 @@ let car1 = new Car('BMW', 120);
 let car2 = new Car('Mercedes', 95);
 car1.accelerate();
 car1.brake();
+
+function EV(make, speed, charge) {
+	Car.call(this, make, speed);
+	this.charge = charge;
+}
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.accelerate = function () {
+	this.speed += 20;
+	this.charge--;
+	console.log(`${this.make} is moving at ${this.speed} km/hr, with a charge ${this.charge}`);
+}
+EV.prototype.brake = function () {
+	this.speed -= 20;
+	this.charge--;
+	console.log(`${this.make} is moving at ${this.speed} km/hr, with a charge ${this.charge}`);
+}
+EV.prototype.chargeBattery = function (chargeTo) {
+	this.charge = chargeTo;
+	console.log(`${this.make} is moving at ${this.speed} km/hr, with a charge ${this.charge}`);
+}
+let elecCar = new EV('Tesla', 120, 23);
+elecCar.accelerate();
+elecCar.chargeBattery(90);
