bc9a7c5140bac069f2c6d9c7895d2a9bbce22510
ahmedwelhakim <ahmed.w.elhakim@gmail.com>
2022-09-01 21:51:47 +0200

    Implementing the first coding Challenge in OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+function Car(make, speed) {
+	this.make = make;
+	this.speed = speed;
+}
+Car.prototype.accelerate = function () { this.speed++; console.log(this.speed); }
+Car.prototype.brake = function () { this.speed--; console.log(this.speed); }
+let car1 = new Car('BMW', 120);
+let car2 = new Car('Mercedes', 95);
+car1.accelerate();
+car1.brake();
